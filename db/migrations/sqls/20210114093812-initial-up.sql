799705ed84cfd9047140ebcb6547addcda0d1d1d
felzek <yukunpengba@gmail.com>
2021-01-14 13:15:10 -0600

    migration setup

--- db/migrations/sqls/20210114093812-initial-up.sql
+++ db/migrations/sqls/20210114093812-initial-up.sql
@@ -1,16 +1,21 @@
 /* Replace with your SQL commands */
-CREATE DATABASE cars;
-
-CREATE TABLE cars (
-    id varchar(25) PRIMARY KEY not null,
-    make varchar(25) NOT NULL,
-    model varchar(25) NOT NULL,
-    color varchar(25) NOT NULL,
+CREATE TABLE IF NOT EXISTS cars (
+    id text UNIQUE PRIMARY KEY NOT NULL,
+    make text NOT NULL,
+    model text NOT NULL,
+    package text NOT NULL,
+    color text NOT NULL,
     year INTEGER NOT NULL,
-    category varchar(40) NOT NULL,
+    category text NOT NULL,
     millege real NOT null,
     price real not null
 );
 
-INSERT INTO cars(id,make,model,color,year,category,millege,price) 
+  CREATE TABLE users (
+    id uuid PRIMARY KEY,
+    email text UNIQUE,
+    password text
+  );
+
+INSERT INTO cars(id,make,model,package,color,year,category,millege,price) 
     VALUES ('JH290XJ','Ford','F10','Base','Silver','2010','Truck',120123, 1999900);
